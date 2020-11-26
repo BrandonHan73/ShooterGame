6b7308ee0d01049e3b0585c685bc6d65d00babe6
Brandon Han <brandontthan2005@gmail.com>
2020-11-26 12:17:48 -0800

    Set up visuals for Player.java

--- src/main/Player.java
+++ src/main/Player.java
@@ -1,4 +1,23 @@
 package main;
 
-public class Player {
+import javax.swing.*;
+
+public class Player extends JPanel {
+
+    // Constants
+    private int gameWidth, gameHeight;
+    private final int frameWidth = 500, frameHeight = 500;
+
+    public Player(int gameWidth, int gameHeight) {
+
+        // Set up constants
+        this.gameWidth = gameWidth;
+        this.gameHeight = gameHeight;
+
+        // Set up frame
+        setSize(frameWidth, frameHeight);
+        setBounds((gameWidth / 2) - (frameWidth / 2), (gameHeight / 2) - (frameHeight / 2), frameWidth, frameHeight);
+
+    }
+
 }
