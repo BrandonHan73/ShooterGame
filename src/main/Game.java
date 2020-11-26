e64e9fa74d9fbe3f7be9638effb33286951793f2
Brandon Han <brandontthan2005@gmail.com>
2020-11-26 15:51:27 -0800

    Added fields to all main classes

---
+++ src/main/Game.java
@@ -0,0 +1,12 @@
+package main;
+
+public class Game {
+
+    private Coords playerCoords;
+    private BulletZone enemyBulletZone, playerBulletZone;
+
+    private Background background;
+    private Player player;
+    private Enemy enemies;
+
+}
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,32 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
+
+    // Constants
+    public static int windowWidth = 1900;
+    public static int windowHeight = 1100;
+
+    private static JFrame mainFrame;
+
+    private static Game mainGame;
+    private static Inventory mainInventory;
+
+    // Inputs
+    private static Keyboard keyboard;
+    private static Mouse mouse;
 
     public static void main(String[] args) {
 
+        // Set up window
+        mainFrame = new JFrame("Shooter Game");
+        mainFrame.setResizable(false);
+        mainFrame.setSize(windowWidth, windowHeight);
+        mainFrame.setLayout(null);
+        mainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        mainFrame.setVisible(true);
 
     }
 
