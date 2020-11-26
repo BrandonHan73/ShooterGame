e1dfd1560144e75a0d20ecdbd91c78a737d598d0
Brandon Han <brandontthan2005@gmail.com>
2020-11-26 00:09:40 -0800

    Set up main window, and created Game.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,30 @@
 package main;
 
+import javax.swing.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    // Constants
 
+
+    // Main window
+    private static JFrame MainFrame;
+
+    // Game
+    private static Game MainGame;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // Set up main window
+        MainFrame = new JFrame("Shooter Game");
+        MainFrame.setSize(1900, 1100);
+        MainFrame.setLayout(null);
+        MainFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        MainFrame.setVisible(true);
+
+        // Set up game;
+        MainGame = new Game();
 
     }
 
