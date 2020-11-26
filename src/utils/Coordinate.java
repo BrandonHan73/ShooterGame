392108c6b732146951aa1bedb6d955773cf8d8c0
Brandon Han <brandontthan2005@gmail.com>
2020-11-26 11:21:47 -0800

    Finished Coordinate.java

--- src/utils/Coordinate.java
+++ src/utils/Coordinate.java
@@ -1,4 +1,59 @@
 package utils;
 
 public class Coordinate {
+
+    private double x, y;
+
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public int getIntX() {
+        return (int)x;
+
+    }
+
+    public int getIntY() {
+        return (int)y;
+
+    }
+
+    public void move(double x, double y) {
+        this.x += x;
+        this.y += y;
+
+    }
+
+    public void moveUp(double y) {
+        this.y -= y;
+
+    }
+
+    public void moveDown(double y) {
+        this.y += y;
+
+    }
+
+    public void moveLeft(double x) {
+        this.x -= x;
+
+    }
+
+    public void moveRight(double x) {
+        this.x += x;
+
+    }
+
+    public double getX() {
+        return x;
+
+    }
+
+    public double getY() {
+        return y;
+
+    }
+
 }
