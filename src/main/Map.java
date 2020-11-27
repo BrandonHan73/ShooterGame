6c86ae4008e536b6fc01febfd639041d8cbe28f1
Brandon Han <brandontthan2005@gmail.com>
2020-11-27 00:08:15 -0800

    Added drawLine to Map.java

--- src/main/Map.java
+++ src/main/Map.java
@@ -15,4 +15,34 @@ public class Map {
 
     }
 
+    public void drawLine(Coords start, Coords end, boolean value) {
+        boolean vertical = (start.getX() == end.getX());
+        double slope = 0;
+        if(!vertical) {
+            slope = (start.getY() - end.getY()) / (start.getX() - end.getX());
+
+        }
+
+        if(slope < 1 && !vertical) {
+            for(int i = (int)start.getX(); i != (int)end.getX(); i += 0) {
+                map[i][(int)(slope * (i - (int)start.getX()) + (int)start.getY())] = value;
+                if(start.getX() < end.getX()) i++;
+                else i--;
+
+            }
+
+        } else {
+            if(!vertical) slope = 1 / slope;
+            for(int i = (int)start.getY(); i != (int)end.getY(); i += 0) {
+                map[(int)(slope * (i - (int)start.getY()) + (int)start.getX())][i] = value;
+                if(start.getY() < end.getY()) i++;
+                else i--;
+
+            }
+
+        }
+        map[(int)end.getX()][(int)end.getY()] = value;
+
+    }
+
 }
